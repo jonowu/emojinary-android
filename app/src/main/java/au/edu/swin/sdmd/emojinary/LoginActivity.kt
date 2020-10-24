1fe84ba8d9f913d5737e7f353b1e74ac95983a68
Jonathan Wu <53251695+jonowu@users.noreply.github.com>
2020-10-24 17:29:00 +1100

    feat: get Firebase auth working

--- app/src/main/java/au/edu/swin/sdmd/emojinary/LoginActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/emojinary/LoginActivity.kt
@@ -2,7 +2,9 @@ package au.edu.swin.sdmd.emojinary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 private const val TAG = "LoginActivity"
@@ -20,6 +22,30 @@ class LoginActivity : AppCompatActivity() {
                 btnLogin.isEnabled = true
                 return@setOnClickListener
             }
+            // Firebase authentication check
+            val auth = FirebaseAuth.getInstance()
+            // This is an async operation, so we need an addOnCompleteListener
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                btnLogin.isEnabled = true
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
+                    goTriviaActivity()
+                } else {
+                    // if login fails, log and toast that it failed with the exception
+                    Log.e(TAG, "signInWithEmail failed", task.exception)
+                    Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+
         }
+    }
+
+    private fun goTriviaActivity() {
+        Log.i(TAG, "goTriviaActivity")
+        /*
+        val intent = Intent(this, TriviaActivity::class.java)
+        startActivity(intent)
+        finish()
+         */
     }
 }
