c69821ab8f05a9d8cef6174c9e31ab40132bcf25
Jonathan Wu <53251695+jonowu@users.noreply.github.com>
2020-10-24 17:46:26 +1100

    feat: add logout ability

---
+++ app/src/main/java/au/edu/swin/sdmd/emojinary/ProfileActivity.kt
@@ -0,0 +1,33 @@
+package au.edu.swin.sdmd.emojinary
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+
+private const val TAG = "ProfileActivity"
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_profile, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_logout) {
+            Log.i(TAG, "User is logging out.")
+            FirebaseAuth.getInstance().signOut()
+            val logoutIntent = Intent(this, LoginActivity::class.java)
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(logoutIntent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
--- app/src/main/java/au/edu/swin/sdmd/emojinary/TriviaActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/emojinary/TriviaActivity.kt
@@ -1,11 +1,30 @@
 package au.edu.swin.sdmd.emojinary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class TriviaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trivia)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // call menuInflater and inflate the menu resource file
+        menuInflater.inflate(R.menu.menu_trivia, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // if the user taps on the profile icon
+        if (item.itemId == R.id.menu_profle) {
+            // navigate to the ProfileActivity using an intent
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
