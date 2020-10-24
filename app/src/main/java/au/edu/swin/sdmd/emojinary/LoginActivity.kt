5aa04a3b60beff54a30f1325f32957938a22d547
Jonathan Wu <53251695+jonowu@users.noreply.github.com>
2020-10-24 17:15:18 +1100

    feat: add login button functionality

--- app/src/main/java/au/edu/swin/sdmd/emojinary/LoginActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/emojinary/LoginActivity.kt
@@ -2,10 +2,24 @@ package au.edu.swin.sdmd.emojinary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
+private const val TAG = "LoginActivity"
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnLogin.setOnClickListener {
+            btnLogin.isEnabled = false
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+            if (email.isBlank() || password.isBlank()) {
+                // Show toast if email or password is empty
+                Toast.makeText(this, "Email/password cannot be empty", Toast.LENGTH_SHORT).show()
+                btnLogin.isEnabled = true
+                return@setOnClickListener
+            }
+        }
     }
 }
