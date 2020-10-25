21522da39d6be2ff215e2b669929cadce0c62861
Jonathan Wu <53251695+jonowu@users.noreply.github.com>
2020-10-25 18:44:11 +1100

    fix: add documentId to Movie class using @DocumentId annotation

--- app/src/main/java/au/edu/swin/sdmd/emojinary/models/Movie.kt
+++ app/src/main/java/au/edu/swin/sdmd/emojinary/models/Movie.kt
@@ -2,10 +2,13 @@ package au.edu.swin.sdmd.emojinary.models
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import com.google.firebase.firestore.DocumentId
 
 @Parcelize
 data class Movie(
+    @DocumentId var documentId: String,
     var emoji: String = "", var difficulty: Int = 0,
     var answers: List<String> = listOf(), var username: String = ""
 ): Parcelable {
 }
+
